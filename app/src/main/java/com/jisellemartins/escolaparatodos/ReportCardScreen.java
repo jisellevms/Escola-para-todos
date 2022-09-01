fd49c2d0f4040435d972f9462e2b930cc6c394e6
jiselle.santana <jisellevms@gmail.com>
2022-09-01 15:10:14 -0300

    Criando a base da tela de boletim

---
+++ app/src/main/java/com/jisellemartins/escolaparatodos/ReportCardScreen.java
@@ -0,0 +1,14 @@
+package com.jisellemartins.escolaparatodos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportCardScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_card_screen);
+    }
+}
