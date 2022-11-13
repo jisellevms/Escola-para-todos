ceacb34ad0957323a056a9cd3fa5d70bfb92eb2c
jiselle.santana <jisellevms@gmail.com>
2022-11-13 20:03:50 -0300

    desenvolvendo tela de calendario

--- app/src/main/java/com/jisellemartins/escolaparatodos/CalendarioScreen.java
+++ app/src/main/java/com/jisellemartins/escolaparatodos/CalendarioScreen.java
@@ -8,9 +8,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.jisellemartins.escolaparatodos.adapter.AdapterConteudo;
 import com.jisellemartins.escolaparatodos.adapter.AdapterEvento;
 import com.jisellemartins.escolaparatodos.dialogs.DialogAdicionarEvento;
@@ -18,15 +25,24 @@ import com.jisellemartins.escolaparatodos.dialogs.DialogAula;
 import com.jisellemartins.escolaparatodos.model.Conteudo;
 import com.jisellemartins.escolaparatodos.model.Evento;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class CalendarioScreen extends AppCompatActivity {
 
     RecyclerView listaEventos;
     Button btnAdcEvento;
+    CalendarView calendar;
+    String disciplinaTime = "";
 
 
     String usuario = aluno;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String dataSelecionada = "";
+    ArrayList<Evento> list = new ArrayList<>();
 
 
     @Override
@@ -36,37 +52,71 @@ public class CalendarioScreen extends AppCompatActivity {
 
         listaEventos = findViewById(R.id.listaEventos);
         btnAdcEvento = findViewById(R.id.btnAdcEvento);
+        calendar = findViewById(R.id.calendar);
 
         SharedPreferences sharedPref = getSharedPreferences("chaves", MODE_PRIVATE);
         usuario = sharedPref.getString("usuario", aluno);
+        disciplinaTime = sharedPref.getString("disciplina", "");
 
-        if (usuario.equals(aluno)){
+
+        if (usuario.equals(aluno)) {
             btnAdcEvento.setVisibility(View.GONE);
-        }else{
+        } else {
             btnAdcEvento.setVisibility(View.VISIBLE);
         }
+
+        calendar.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            list.clear();
+            dataSelecionada = year + "/" + (month + 1) + "/" + dayOfMonth;
+            SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+            try {
+                Date date = format.parse(dataSelecionada);
+
+
+                db.collection("Evento")
+                        .whereEqualTo("disciplina", disciplinaTime)
+                        .whereEqualTo("dateEvento", String.valueOf(date.getTime()))
+                        .get()
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                verificaEventoExiste(task);
+                            } else {
+                                Toast.makeText(this, "Erro: " + task.getException(), Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        });
 
         btnAdcEvento.setOnClickListener(view -> {
             DialogAdicionarEvento alert = new DialogAdicionarEvento();
             alert.showDialog(this);
         });
 
-        ArrayList<Evento> list = new ArrayList<>();
+    }
 
-        Evento evento = new Evento();
-        evento.setData("09/06");
-        evento.setDescricao("Aula extra de revisão");
+    public void verificaEventoExiste(Task<QuerySnapshot> task) {
+        if (task.getResult().size() == 0) {
+            listaEventos.setAdapter(new AdapterEvento(this, list));
+            RecyclerView.LayoutManager layout = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
 
-        Evento evento2 = new Evento();
-        evento2.setData("09/06");
-        evento2.setDescricao("Aula extra de revisão");
+            listaEventos.setLayoutManager(layout);
+            Toast.makeText(this, "Não existe evento para a data selecionada", Toast.LENGTH_SHORT).show();
+        } else {
+            for (QueryDocumentSnapshot document : task.getResult()) {
+                Evento evento = new Evento();
+                evento.setData(document.get("dateEvento").toString());
+                evento.setDescricao(document.get("descricao").toString());
+                list.add(evento);
+            }
 
-        list.add(evento);
-        list.add(evento2);
+            listaEventos.setAdapter(new AdapterEvento(this, list));
+            RecyclerView.LayoutManager layout = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
 
-        listaEventos.setAdapter(new AdapterEvento(this, list));
-        RecyclerView.LayoutManager layout = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-
-        listaEventos.setLayoutManager(layout);
+            listaEventos.setLayoutManager(layout);
+        }
     }
 }
+
--- app/src/main/java/com/jisellemartins/escolaparatodos/dialogs/DialogAdicionarEvento.java
+++ app/src/main/java/com/jisellemartins/escolaparatodos/dialogs/DialogAdicionarEvento.java
@@ -1,22 +1,89 @@
 package com.jisellemartins.escolaparatodos.dialogs;
 
+import static android.content.Context.MODE_PRIVATE;
+import static com.jisellemartins.escolaparatodos.CadastroScreen.getRandomNonRepeatingIntegers;
+import static com.jisellemartins.escolaparatodos.Utils.UtilAutenticacao.aluno;
+
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.SharedPreferences;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.jisellemartins.escolaparatodos.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class DialogAdicionarEvento {
+
+    String dataSelecionada = "";
+    String disciplinaTime = "";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
     public void showDialog(Activity activity){
         final Dialog dialog = new Dialog(activity);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setCancelable(false);
         dialog.setContentView(R.layout.dialog_adicionar_evento);
 
-        //Button dialogButton = (Button) dialog.findViewById(R.id.btnIniciarAula);
         ImageView cancelar = (ImageView) dialog.findViewById(R.id.cancelar);
+        Button btnSalvar = (Button) dialog.findViewById(R.id.btnSalvar);
+        CalendarView calendar = (CalendarView) dialog.findViewById(R.id.calendar);
+        EditText campoDesc = (EditText) dialog.findViewById(R.id.campoDesc);
+
+        SharedPreferences sharedPref = activity.getSharedPreferences("chaves", MODE_PRIVATE);
+        disciplinaTime = sharedPref.getString("disciplina", "");
+
         cancelar.setOnClickListener(v -> dialog.dismiss());
+
+        calendar.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            dataSelecionada = year + "/" + (month+1) + "/" + dayOfMonth;
+        });
+
+        btnSalvar.setOnClickListener(view -> {
+            if (dataSelecionada.equals("") || campoDesc.getText().toString().isEmpty()){
+                Toast.makeText(activity, "É necesssário selecionar uma data e preencher o campo.", Toast.LENGTH_LONG).show();
+            }else{
+                SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+                try {
+                    Date date = format.parse(dataSelecionada);
+
+                    Map<String, Object> professor = new HashMap<>();
+                    professor.put("dateEvento", String.valueOf(date.getTime()));
+                    professor.put("descricao", campoDesc.getText().toString());
+                    professor.put("disciplina", disciplinaTime);
+
+                    db.collection("Evento").document(getRandomNonRepeatingIntegers(6,0,1000).toString())
+                            .set(professor)
+                            .addOnSuccessListener(aVoid -> {
+                                Toast.makeText(activity, "Evento adicionado com sucesso!", Toast.LENGTH_LONG).show();
+                                dialog.dismiss();
+
+                            })
+                            .addOnFailureListener(e -> {
+                                Toast.makeText(activity, "Erro: " + e, Toast.LENGTH_LONG).show();
+
+                            });
+
+
+                } catch (ParseException e) {
+                    Toast.makeText(activity, "Erro: " + e, Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
 
         dialog.show();
 
